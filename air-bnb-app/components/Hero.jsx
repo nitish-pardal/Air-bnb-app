ab67d640aef37db239bf92c0098d9f8f0a1547ca
nitish-pardal <nitishpardal@gmail.com>
2023-11-13 13:06:12 +0530

    13-11-2023

--- air-bnb-app/components/Hero.jsx
+++ air-bnb-app/components/Hero.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import groupImages from '../src/assets/images-group.png';
 
 export default function Hero(){
-  
+  return(
+  <div className="hero--container">
+    <img src={groupImages} alt=" hero-images" className="Hero--image" />
+  </div>
+  );
 }
--- air-bnb-app/src/main.jsx
+++ air-bnb-app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import Navbar from "../components/Navbar";
+import Hero from '../components/Hero';
 import "./index.css";
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <div>
      <Navbar />
+     <Hero />
   </div>
  
 );
